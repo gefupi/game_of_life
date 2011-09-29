9f240b8eeff4ffc7324404b4ecc4fdc9a333b3d2
Flash Byte <mail@flash-byte.de>
2011-09-29 18:42:23 +0200

    Add the Constructor for the biotope object

--- biotope.c
+++ biotope.c
@@ -1,2 +1,13 @@
 #include "biotope.h"
+#include "life_point.h"
 
+biotope* init_biotope(int max_x,int max_y){
+	biotope* this = (biotope*)malloc(sizeof(biotope));
+	if ((max_x <=0)||(max_y<=0)){
+		free(this);
+		fprintf(stderr, "X or Y fucked up in init_biotope\n");
+		exit(EXIT_FAILURE);
+	}
+	this->max_x->max_x;
+	this->max_y->max_y;
+}
--- biotope.h
+++ biotope.h
@@ -6,3 +6,12 @@ typedef struct _biotope_{
 	struct _life_point_ **field;
 } biotope;
 
+/* Constuctor for the "biotop onject"
+ * @input:
+ *		max_x - max. width of biotope. if zero given determined by file
+ *		max_y - max. heigth of biotope. if zero given determined by file
+ *		filename - char pointer to biotope-filename
+ * @return:
+ 		pointer to biotope 
+*/
+biotope* init_biotope(int max_x,int max_y,char* filename);
